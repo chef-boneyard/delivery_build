aec5c92f0ab56dc84d85f9b1bd2186655a36af4d
Tom Duffield <tom@chef.io>
2015-03-23 12:46:16 -0500

    Add LC_ALL to address berkshelf-api#112

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam@chef.io'
 license          'apache2'
 description      'Sets up a delivery build node'
 long_description 'Sets up a delivery build node'
-version          '0.1.3'
+version          '0.1.4'
 
 depends 'git'
 depends 'packagecloud'
