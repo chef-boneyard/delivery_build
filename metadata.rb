30ba3cfc663ea5316f77e3e0a7df15842c13ca2b
Jon Anderson <janderson@chef.io>
2015-05-06 22:07:24 -0400

    Bump metadata version for rustup fix to cli.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam@chef.io'
 license          'apache2'
 description      'Sets up a delivery build node'
 long_description 'Sets up a delivery build node'
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'git'
 depends 'packagecloud'
