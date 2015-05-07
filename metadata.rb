11d644ce3376df4500c6ac7e7bdd134e619b4f62
Jon Morrow <jon@morrowmail.com>
2015-05-07 15:16:56 -0700

    Check for empty string per @jean

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam@chef.io'
 license          'apache2'
 description      'Sets up a delivery build node'
 long_description 'Sets up a delivery build node'
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'git'
 depends 'packagecloud'
