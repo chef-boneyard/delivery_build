f92552f32e8bbbe7b6d7a17c910d612e9ab213ab
Matt Campbell <mcampbell@chef.io>
2015-11-02 14:51:44 -0600

    Remove support for compiling delivery-cli from source
    This feature was only used in development and is moving to the
    delivery-dev cookbook.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'apache2'
 description 'Sets up a delivery build node'
-version '0.3.4'
+version '0.4.0'
 
 depends 'git'
 depends 'build-essential'
