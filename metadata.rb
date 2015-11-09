9d6124f22e1ef3ae1deaef902b2499a0ba5ca374
David McCown <dmccown@chef.io>
2015-11-09 14:28:07 -0600

    Bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'apache2'
 description 'Sets up a delivery build node'
-version '0.4.5'
+version '0.4.6'
 
 depends 'git'
 depends 'build-essential'
