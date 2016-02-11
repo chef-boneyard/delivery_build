635e553feefe670a334c420e898fa3dc862d21a8
Victoria Jeffrey <vjeffrey@chef.io>
2016-02-11 11:07:41 -0500

    Push chef-stacktrace.out to streamy on failed chef-client runs

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'apache2'
 description 'Sets up a delivery build node'
-version '0.4.15'
+version '0.4.16'
 
 depends 'git'
 depends 'build-essential'
