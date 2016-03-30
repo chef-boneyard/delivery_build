b2876de606b8702b30bcbf5e8b8eedcee595cd9b
Seth Chisamore <schisamo@chef.io>
2016-03-30 14:45:01 -0400

    Add Windows support to Serverspec test coverage

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'delivery_build::default' do
+context 'on Unix', if: !windows? do
   # Push-Jobs
   describe service('opscode-push-jobs-client') do
     it { should be_running }
@@ -46,47 +46,84 @@ describe 'delivery_build::default' do
 
   # Workspace
   describe 'Workspace Configuration' do
-    if %w(debian ubuntu redhat centos).include?(os[:family])
-      context user('dbuild') do
-        it { should exist }
-        it { should have_home_directory '/var/opt/delivery/workspace' }
-      end
+    context user('dbuild') do
+      it { should exist }
+      it { should have_home_directory '/var/opt/delivery/workspace' }
+    end
 
-      context file('/var/opt/delivery/workspace') do
-        it { should be_directory }
+    context file('/var/opt/delivery/workspace') do
+      it { should be_directory }
+      it { should be_owned_by 'dbuild' }
+    end
+
+    context file('/var/opt/delivery/workspace/.chef') do
+      it { should be_directory }
+      it { should be_owned_by 'dbuild' }
+    end
+
+    %w(
+      /var/opt/delivery/workspace/.chef/builder_key
+      /var/opt/delivery/workspace/.chef/delivery.pem
+      /var/opt/delivery/workspace/.chef/knife.rb
+      /var/opt/delivery/workspace/etc/builder_key
+      /var/opt/delivery/workspace/etc/delivery.pem
+      /var/opt/delivery/workspace/etc/delivery.rb
+    ).each do |dbuild_file|
+      context file(dbuild_file) do
+        it { should be_file }
         it { should be_owned_by 'dbuild' }
       end
+    end
 
-      context file('/var/opt/delivery/workspace/.chef') do
-        it { should be_directory }
-        it { should be_owned_by 'dbuild' }
+    %w(
+      /var/opt/delivery/workspace/bin/git_ssh
+      /var/opt/delivery/workspace/bin/delivery-cmd
+    ).each do |root_file|
+      context file(root_file) do
+        it { should be_file }
+        it { should be_owned_by 'root' }
       end
+    end
+  end
+end
 
-      %w(
-        /var/opt/delivery/workspace/.chef/builder_key
-        /var/opt/delivery/workspace/.chef/delivery.pem
-        /var/opt/delivery/workspace/.chef/knife.rb
-        /var/opt/delivery/workspace/etc/builder_key
-        /var/opt/delivery/workspace/etc/delivery.pem
-        /var/opt/delivery/workspace/etc/delivery.rb
-      ).each do |dbuild_file|
-        context file(dbuild_file) do
-          it { should be_file }
-          it { should be_owned_by 'dbuild' }
+describe 'on Windows', if: windows? do
+  # ChefDK
+  describe 'ChefDK' do
+    context command('chef -v') do
+      its(:stdout) { should match(/Chef Development Kit/) }
+      its(:exit_status) { should eq 0 }
+    end
+  end
+
+  # Git
+  describe 'git' do
+    context command('git --version') do
+      its(:stdout) { should match(/git version 2.7.4/) }
+      its(:exit_status) { should eq 0 }
+    end
+  end
+
+  # Workspace
+  describe 'Workspace Configuration' do
+    %w(
+      C:/delivery/ws
+      C:/delivery/ws/.chef
+      C:/delivery/ws/.chef/builder_key
+      C:/delivery/ws/.chef/delivery.pem
+      C:/delivery/ws/.chef/knife.rb
+      C:/delivery/ws/bin/git_ssh
+      C:/delivery/ws/bin/delivery-cmd
+      C:/delivery/ws/etc/builder_key
+      C:/delivery/ws/etc/delivery.pem
+      C:/delivery/ws/etc/delivery.rb
+    ).each do |workspace_file|
+      # We are using regular Ruby because ServerSpec existent checks are failing on Windows
+      describe workspace_file do
+        it 'exists' do
+          expect(File.exist?(workspace_file)).to be true
         end
       end
-
-      %w(
-        /var/opt/delivery/workspace/bin/git_ssh
-        /var/opt/delivery/workspace/bin/delivery-cmd
-      ).each do |root_file|
-        context file(root_file) do
-          it { should be_file }
-          it { should be_owned_by 'root' }
-        end
-      end
-      # TODO: Windows tests
-      # else # Windows
     end
   end
 end
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -5,5 +5,9 @@ if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
 else
   set :backend, :cmd
   set :os, family: 'windows'
-  set :path, 'C:/Program Files (x86)/Git/Cmd;C:/Program Files (x86)/Git/libexec/git-core;C:/Opscode/chefdk/bin;C:/Opscode/chefdk/embedded/bin'
+  set :path, 'C:/Program Files (x86)/Git/Cmd;C:/Program Files (x86)/Git/libexec/git-core;C:/Opscode/chefdk/bin;'
+end
+
+def windows?
+  os[:family] == 'windows'
 end
